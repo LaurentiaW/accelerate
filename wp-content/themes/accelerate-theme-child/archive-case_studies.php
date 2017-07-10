ce461eb0c959d30ef8422e9aae922676a818b018
laurentiaw <laurentiawehmeyer@gmail.com>
2017-07-10 11:17:03 +0200

    create advance custom fields and case studies homepage

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * The template for displaying Case Study pages
+ * The template for displaying archive page for all the case studies
  *
- * *
+ *
  * @package WordPress
  * @subpackage Accelerate Marketing
  * @since Accelerate Marketing 1.0
@@ -11,53 +11,37 @@
 get_header(); ?>
 
 	<div id="primary" class="site-content">
-
 		<div id="content" role="main">
-
 			<?php while ( have_posts() ) : the_post(); 
-				$services = get_field('services');
-				$client = get_field('client');
-				$link = get_field('site_link');
 				$image_1 = get_field('image_1');
-				$image_2 = get_field('image_2');
-				$image_3 = get_field('image_3');
-				$size = "full"
+				$size = "full";
+				$services = get_field('services');
+				$link = get_field('site_link');
 			?>
-
 			<article class="case-study">
 				<aside class="case-study-sidebar">
 					<h2><?php the_title();?></h2>
 					<h5><?php echo $services; ?></h5>
-					<h6>Client: <?php echo $client; ?></h6>
-
+					
 					<?php the_content(); ?>
 
-					<p><strong><a href="<?php echo $link; ?>">Site link</a></strong></p>
+					<p class="hover-link"><strong><a href="<?php echo $link ; ?>">View Project &gt;</a></strong></p>
 				</aside>
 
-				<div class="case-study-images">
+				<div class="case-study-images-archivepage">
 					<?php if ($image_1) { 
 						echo wp_get_attachment_image( $image_1, $size );
 					 } ?>
 					
-					 <?php if ($image_2) { 
-						echo wp_get_attachment_image( $image_2, $size );
-					 } ?>
-
-					 <?php if ($image_3) { 
-						echo wp_get_attachment_image( $image_3, $size );
-					 } ?>
-					
+							
 				</div>
 
 
 			</article>
-		
+	
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
-
 <?php get_footer(); ?>
-
--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -25,8 +25,39 @@ get_header(); ?>
 	</div><!-- .container -->
 </section><!-- .home-page -->
 
+<section class="featured-work">
+   <div class="site-content">
+    <div class="fwork">
+      
+      <h4>Featured Work</h4>
+      <div class="homep-fwork">
+        <ul class="homepage-featured-work">  
+          <?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+          <?php while ( have_posts() ) : the_post(); 
+
+              $image_1 = get_field("image_1");
+              $size = "medium"
+          ?>
+          <li class="individual-featured-work">
+              <figure>
+                <?php echo wp_get_attachment_image($image_1, $size); ?>
+              </figure>
+
+            <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+          </li>
+          <?php endwhile; ?> 
+          <?php wp_reset_query(); ?>
+        </ul>
+      </div>
+    </div>
+   </div>
+  
+
+</section>
+
+
 <section class="recent-posts">
- <div class="site-content">
+<div class="site-content">
   <div class="blog-post">
    <h4>From the Blog</h4>
     <?php query_posts('posts_per_page=1'); ?>
@@ -35,7 +66,7 @@ get_header(); ?>
        <?php the_excerpt(); ?> 
        <a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
      <?php endwhile; ?> 
-    <?php wp_reset_query(); ?>
+
    </div>
   </div>
 </section>
