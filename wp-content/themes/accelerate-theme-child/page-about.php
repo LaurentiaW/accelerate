5067a0f24129bcf032bf8ff88193ebe88564d7c1
laurentiaw <laurentiawehmeyer@gmail.com>
2017-07-28 17:11:34 +0200

    added the services to the about page- need to still get them to alternate based on order

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying about page
+ * The template for displaying the about page
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -14,39 +14,55 @@
 
 get_header(); ?>
 
+
+<section class="about-page">
+  <div class="site-content">
+    <?php while ( have_posts() ) : the_post(); ?>
+      <div class='about-hero'>
+        <?php the_content(); ?>
+       </div>
+    <?php endwhile; // end of the loop. ?>
+  </div><!-- .container -->
+</section><!-- .home-page -->
+
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<h2><?php the_title (); ?></h2>
-				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
-
-			<h4>our services</h4>
+			<h4>Our Services</h4>
 			<p id="services-p">We take pride in our clients and the content we create for them. 
-Here’s a brief overview of our offered services.</p>
-      <div class="list-of-services">
-        <ul class="list-services">
-        <!--need to find the right way to right this query post-->  
-          <?php query_posts('posts_per_page=6&post_type=services'); ?>
+        Here’s a brief overview of our offered services.</p>
+    </div>
+
+    <section class="list-services">
+      <?php query_posts('post_type=services'); ?>
           <?php while ( have_posts() ) : the_post(); 
 
               $icon = get_field("icon");
               $size = "medium"
           ?>
-          <li class="individual-service">
-              <figure class="icon">
-                <?php echo wp_get_attachment_image($image_1, $size); ?>
-              </figure>
-              <div id="service-descrip">
-            	<h5><?php the_title(); ?></h5>
-            	<p><?php the_content(); ?></p>
+
+            <div id="alternating-content">
+
+              <div class="item">  
+                <figure class="icon">  <?php echo wp_get_attachment_image($icon, $size); ?>  </figure>
+                <div class="caption">  
+                    <ul>
+                      <li><h3><?php the_title(); ?></h3></li>
+                      <li><p><?php the_content(); ?></p></li>
+                    </ul>
+                  </div>  
+                </div>
             </div>
-          </li>
+
+               
+   
+             
+        
           <?php endwhile; ?> 
           <?php wp_reset_query(); ?>
-        </ul>
-      </div>
 
+    </section>
+         
+ 
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
